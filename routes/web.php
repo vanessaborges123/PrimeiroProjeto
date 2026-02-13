0ed3760bf6ead3e668fbc0f0f9a9320413113ea9
Vanessa Borges <vanessaborges123@gmail.com>
2026-02-12 22:00:57 -0300

    Primeiro Commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::get('/paginainicial', function () {
+    return view('welcome');
+});
+
+Route::get('/exercicio', function() {
+    return view('exercicio');
+});
+
+Route::post('/resposta', function(Request $request) {
+    $valor1 = $request->input('valor1');
+    $valor2 = $request->input('valor2');
+    $soma = $valor1 + $valor2;
+    return("A soma é: $soma");
+});
